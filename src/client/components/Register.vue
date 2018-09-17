1fc3468197eb10c4df9dc76306fea590261930f1
John <darkneter@gmail.com>
2018-09-17 10:01:26 -0700

    update register checks. Remove backup file.

--- src/client/components/Register.vue
+++ src/client/components/Register.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
-        <div class="example">{{ msg }}</div>
-        <div>
-            <table>
-                <tr>
-                    <td>User:</td>
-                    <td><input v-model="username"></td>
-                </tr>
-                <tr>
-                    <td>Password:</td>
-                    <td><input v-model="passphrase"></td>
-                </tr><tr>
-                    <td>
-                    </td><td style=" text-align: center;">
-                        <button @click="clickSubmit">Submit</button>
-                        <button @click="clickCancel">Cancel</button>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        
+        <strong v-if="bregister">
+            <div class="example">{{ msg }}</div>
+            <div>
+                <table>
+                    <tr>
+                        <td>User:</td>
+                        <td><input v-model="username"></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input v-model="passphrase"></td>
+                    </tr><tr>
+                        <td>
+                        </td><td style=" text-align: center;">
+                            <button @click="clickSubmit">Submit</button>
+                            <button @click="clickCancel">Cancel</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </strong>
+        <strong v-else>
+            {{msg}} <button @click="clickCancel">Home</button>
+        </strong>
     </div>
 </template>
 
@@ -30,30 +34,27 @@ export default {
             msg: 'Register!',
             username:'guest',
             passphrase:'guest',
+            bregister:true,
         }
     },
     methods: {
         clickSubmit(){
             console.log("login...");
-            console.log(this.username);
-            console.log(this.$gun);
+            //console.log(this.username);
+            //console.log(this.$gun);
             var user = this.$gun.user();
+            var self = this;
 
             user.create(this.username, this.passphrase, function(ack){
                 //console.log(ack);
                 if(ack.err){
-                    console.log("fail!");
-                    //self.$toast.open({
-                        //message:ack.err ,
-                        //type: 'is-danger'
-                    //});
+                    //console.log("fail!");
+                    self.msg = "Register Alias Fail!";
                 }
                 if(ack.pub){
-                    console.log("created!", ack.pub);
-                    //self.$toast.open({
-                        //message: 'Alias Created: ' + params.username +'!',
-                        //type: 'is-success'
-                    //});
+                    //console.log("created!", ack.pub);
+                    self.msg = "Alias Created!";
+                    self.bregister = false;
                 }
             });
         },
